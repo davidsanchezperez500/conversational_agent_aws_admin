1bf3135f41c45dd3831be266f7a05eb265e8f2e0
David Sánchez <david.sanchez.vvjj@gmail.com>
2025-10-15 08:48:56 +0200

    feat: config backend

---
+++ cloudwatch.tf
@@ -0,0 +1,81 @@
+resource "aws_cloudwatch_log_group" "s3_trail_logs" {
+  name              = "/aws/cloudtrail/s3-logs"
+  retention_in_days = 30
+  kms_key_id        = aws_kms_key.conversational_agent_terraform_state.arn
+  tags              = local.aws_common_tags
+}
+
+resource "aws_iam_role" "cloudtrail_to_cw" {
+  name               = "cloudtrail-to-cloudwatch-logs"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "cloudtrail.amazonaws.com" },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudtrail_to_cw" {
+  name   = "cloudtrail-to-cloudwatch-logs-policy"
+  role   = aws_iam_role.cloudtrail_to_cw.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup"
+      ],
+      "Resource": "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/cloudtrail/s3-logs:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/cloudtrail/s3-logs:*"
+    }
+  ]
+}
+EOF
+}
+
+
+/* resource "aws_cloudtrail" "s3_trail_conversational_agent_terraform_state" {
+  name                          = "s3-trail"
+  s3_bucket_name                = aws_s3_bucket.conversational_agent_terraform_state.id
+  include_global_service_events = false
+  is_multi_region_trail         = false
+  enable_logging                = true
+  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.s3_trail_logs.arn
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_to_cw.arn
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = false
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3:::${aws_s3_bucket.conversational_agent_terraform_state.id}/*"]
+    }
+  }
+  tags = local.aws_common_tags
+
+  depends_on = [
+    aws_cloudwatch_log_group.s3_trail_logs,
+    aws_iam_role.cloudtrail_to_cw,
+    aws_iam_role_policy.cloudtrail_to_cw
+  ]
+} */
+
+output "cloudwatch_log_group_arn" {
+  value = aws_cloudwatch_log_group.s3_trail_logs.arn
+}
+# arn:aws:logs:us-east-1:853878127117:log-group:/aws/cloudtrail/s3-logs
--- s3.tf
+++ s3.tf
@@ -59,3 +59,37 @@ resource "aws_s3_bucket_lifecycle_configuration" "conversational_agent_terraform
     }
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.conversational_agent_terraform_state.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AWSCloudTrailAclCheck",
+      "Effect": "Allow",
+      "Principal": { "Service": "cloudtrail.amazonaws.com" },
+      "Action": "s3:GetBucketAcl",
+      "Resource": "${aws_s3_bucket.conversational_agent_terraform_state.arn}"
+    },
+    {
+      "Sid": "AWSCloudTrailWrite",
+      "Effect": "Allow",
+      "Principal": { "Service": "cloudtrail.amazonaws.com" },
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.conversational_agent_terraform_state.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
