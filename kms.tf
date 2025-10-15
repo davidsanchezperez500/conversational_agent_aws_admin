76ab0f6148db2dcd424a480143be120a8019bd1a
David Sánchez <39903115+davidsanchezperez500@users.noreply.github.com>
2025-10-15 09:15:36 +0200

    feat: config backend (#3)

--- kms.tf
+++ kms.tf
@@ -16,6 +16,31 @@ resource "aws_kms_key" "conversational_agent_terraform_state" {
         },
         "Action": "kms:*",
         "Resource": "*"
+      },
+      {
+        "Sid": "Allow CloudTrail to use the key",
+        "Effect": "Allow",
+        "Principal": { "Service": "cloudtrail.amazonaws.com" },
+        "Action": [
+          "kms:GenerateDataKey*",
+          "kms:Decrypt"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "Allow CloudWatch Logs to use the key",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "logs.${var.aws_region}.amazonaws.com"
+        },
+        "Action": [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        "Resource": "*"
       }
     ]
   }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "environment" {
   description = "The environment for the deployment (e.g. dev, staging, prod)"
   type        = string
 }
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
